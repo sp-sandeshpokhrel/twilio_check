9fab86d01d536fb0cc383b663709181aac078692
Sandesh Pokhrel <sandesh.pokhrel33@gmail.com>
2023-07-21 11:59:58 +0545

    database integration to twilio service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TwilioModule } from './utils/twilio-service';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [
-    TwilioModule.forRoot({
-      accountSid: process.env.TWILIO_SID,
-      authToken: process.env.TWILIO_SECRET,
-      from: '+14155238886',
-    }),
+    TwilioModule.forRoot(
+      {
+        accountSid: process.env.TWILIO_SID,
+        authToken: process.env.TWILIO_SECRET,
+        from: '+14155238886',
+      },
+      new PrismaService(),
+    ),
   ],
   controllers: [AppController],
   providers: [AppService],
